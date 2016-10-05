746999341df775ab22fcd662c4f11fb9f2529d5f
Natales Aprama <natales.14421019@student.ubl.ac.id>
2016-10-05 23:53:36 +0700

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,20 @@
+package ru1iz.cobalagi;
+
+/**
+ * Created by Narama on 10/5/2016.
+ */
+public class Controller {
+    public String isi;
+
+    public String getIsi() {
+        return isi;
+    }
+
+    public void setIsi(String isi) {
+        this.isi = isi;
+    }
+
+    public  String toString(){
+        return  (getIsi());
+    }
+}
